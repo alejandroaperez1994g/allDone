6bc3b91164414b06ab4c8b85728def8238fdfdcd
Alejandro <alejandroaperez1994g@gmail.com>
2021-10-08 00:31:17 +0200

    adding word classifier to the tasks

--- src/views/Todo.js
+++ src/views/Todo.js
@@ -1,9 +1,14 @@
 import React, { useState } from "react";
 import "../views/Todo.css";
 import db from "../firebase";
-import { Modal } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import DeleteIcon from "@mui/icons-material/Delete";
+import Chip from "@mui/material/Chip";
+import AlternateEmailIcon from "@mui/icons-material/AlternateEmail";
+import LinkIcon from "@mui/icons-material/Link";
+import TagIcon from "@mui/icons-material/Tag";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import Checkbox from "@mui/material/Checkbox";
 
 const useStyles = makeStyles((theme) => ({
   modal: {
@@ -31,92 +36,85 @@ function Todo(props) {
   const [input, setInput] = useState("");
   const [todo, setTodo] = useState([]);
 
-  const updateTodo = () => {
-    db.collection("todos")
-      .doc(props.todo.id)
-      .set({ text: input }, { merge: true });
-
-    setOpen(false);
-  };
-
   let regExEmail =
     /(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/;
   let regExUrl =
     /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g;
-  let arr = [];
 
-  const checkWord = (word) => {
-    if (word.match(regExEmail)) {
-      return "Email";
-    } else if (word.includes("#")) {
-      return "Hastag";
-    } else if (word.includes("@")) {
-      return "Tag";
-    } else if (word.match(regExUrl)) {
-      return "Link";
+  const classify = (todo) => {
+    if (todo.includes("#")) {
+      return (
+        <Chip
+          key={todo}
+          label={todo.substr(1)}
+          color="secondary"
+          size="small"
+          variant="filled"
+          icon={<TagIcon />}
+          sx={{ marginRight: 0.2 }}
+        />
+      );
+    } else if (todo.match(regExEmail)) {
+      return (
+        <Chip
+          key={todo}
+          label={todo}
+          color="warning"
+          size="small"
+          variant="filled"
+          icon={<MailOutlineIcon />}
+          sx={{ marginRight: 0.2 }}
+        />
+      );
+    } else if (todo.includes("@")) {
+      return (
+        <Chip
+          key={todo}
+          label={todo.substr(1)}
+          color="success"
+          size="small"
+          variant="filled"
+          icon={<AlternateEmailIcon />}
+          sx={{ marginRight: 0.2 }}
+        />
+      );
+    } else if (todo.match(regExUrl)) {
+      return (
+        <Chip
+          key={todo}
+          label={todo}
+          color="info"
+          size="small"
+          variant="filled"
+          icon={<LinkIcon />}
+          sx={{ marginRight: 0.2 }}
+        />
+      );
     } else {
-      return word;
+      return <h4>{todo}</h4>;
     }
   };
 
-  const classifyToDO = (todo) => {
-    let todoSplit = todo.split(" ");
-    todoSplit.map((word) => {
-      arr.push(checkWord(word));
-    });
-    return arr.join(" ");
-    // console.log(arr.join(" "));
-  };
-
-  // let date = new Date(props.todo.date.seconds);
-  // console.log(date);
-
+  const label = { inputProps: { "aria-label": "Checkbox demo" } };
   return (
-    <>
-      <Modal
-        className={classes.paper}
-        style={{ alignItems: "center", justifyContent: "center" }}
-        open={open}
-        onClose={(e) => setOpen(false)}
-        aria-labelledby="simple-modal-title"
-        aria-describedby="simple-modal-description"
-      >
-        <div>
-          <h1>Edita tu nota</h1>
-          <input
-            value={input}
-            placeholder={props.todo.todo}
-            onChange={(event) => setInput(event.target.value)}
-          />
-          <button
-            disabled={!input}
-            className="button_edit"
-            onClick={updateTodo}
-          >
-            Close
-          </button>
+    <div className="body">
+      <div class="card">
+        <Checkbox
+          {...label}
+          onClick={(event) =>
+            db.collection("todos").doc(props.todo.id).delete()
+          }
+        />
+        <div class="card-info">
+          {props.todo.todo.split(" ").map((item) => {
+            return classify(item);
+          })}
         </div>
-      </Modal>
-      <div className="body">
-        <div class="card">
-          <div class="card-info">
-            {/* <h2>{`${props.todo.todo}`}</h2> */}
-            {/* {classifyToDO(props.todo.todo)} */}
-            <div className="buttons"></div>
-            <DeleteIcon
-              sx={{ cursor: "Pointer" }}
-              onClick={(event) =>
-                db.collection("todos").doc(props.todo.id).delete()
-              }
-            />
-            <h3>{`${classifyToDO(props.todo.todo)}`}</h3>
-          </div>
-          <div class="date">
-            {new Date(props.todo.date.seconds * 1000).toLocaleString()}
-          </div>
+        <div class="date">
+          {new Date(props.todo.date?.seconds * 1000).toLocaleString()}
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
