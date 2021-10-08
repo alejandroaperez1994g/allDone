852666bc0620fa53f956e08c6e9d315f9d2de3e7
Alejandro <alejandroaperez1994g@gmail.com>
2021-10-09 01:38:23 +0200

    adding some tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import { classify } from "../src/views/Todo";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Tasks/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test("classify function", () => {
+  const text = classify("carta");
+  expect(text).toBe("carta");
 });
